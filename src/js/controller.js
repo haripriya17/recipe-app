782ebe4c66559dba8d26906625e875f2b667f071
haripriya17 <haripriyamekala517@gmail.com>
2024-04-23 20:01:29 -0500

    added fraction library

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-import icons from 'url:../img/icons.svg';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import recipeView from './views/recipeView.js';
@@ -17,23 +17,15 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-const renderSpinner = function (parentEl) {
-  const markup = ` <!-- <div class="spinner">
-  <svg>
-    <use href="${icons}#icon-loader"></use>
-  </svg>
-</div>`;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentElement('afterbegin', markup);
-};
-const showRecipe = async function () {
+
+const controlRecipes = async function () {
   try {
     const id = widow.location.hash.slice(1);
     console.log(id);
 
     if (!id) return;
     //loading recipe
-    renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
 
     await model.loadRecipe(id);
 
@@ -45,5 +37,7 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-['hashchange', 'load'].forEach(e => window.addEventListener(ev, showRecipe));
+['hashchange', 'load'].forEach(e =>
+  window.addEventListener(ev, controlRecipes)
+);
 // showRecipe();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,17 +1,39 @@
+import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
+console.log(Fraction);
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
 
   render(data) {
     this.#data = data;
+    const markup = this.#generateMarkup;
+    this.#clear();
+    this.#parentElement.insertAdjacentElement('afterbegin', markup);
   }
+
+  #clear() {
+    this.#parentElement.innerHTML = '';
+  }
+
+  renderSpinner = function () {
+    const markup = ` <!-- <div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>`;
+    this.#parentElement.innerHTML = '';
+    this.#parentElement.insertAdjacentElement('afterbegin', markup);
+  };
 
   #generateMarkup() {
     return `
   <figure class="recipe__fig">
-    <img src="${recipe.image}" alt=${recipe.title} class="recipe__img" />
+    <img src="${this.#data.image}" alt=${
+      this.#data.title
+    } class="recipe__img" />
     <h1 class="recipe__title">
-      <span>${recipe.title}</span>
+      <span>${this.#data.title}</span>
     </h1>
   </figure>
 
@@ -21,7 +43,7 @@ class RecipeView {
         <use href="${icons}#icon-clock"></use>
       </svg>
       <span class="recipe__info-data recipe__info-data--minutes">${
-        recipe.cookingTime
+        this.#data.cookingTime
       }</span>
       <span class="recipe__info-text">minutes</span>
     </div>
@@ -30,7 +52,7 @@ class RecipeView {
         <use href="${icons}#icon-users"></use>
       </svg>
       <span class="recipe__info-data recipe__info-data--people">${
-        recipe.servings
+        this.#data.servings
       }</span>
       <span class="recipe__info-text">servings</span>
 
@@ -63,21 +85,7 @@ class RecipeView {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
-    ${recipe.ingredients
-      .map(ing => {
-        return `<li class="recipe__ingredient">
-      <svg class="recipe__icon">
-        <use href="src/img/icons.svg#icon-check"></use>
-      </svg>
-      <div class="recipe__quantity">${ing.quantity}</div>
-      <div class="recipe__description">
-        <span class="recipe__unit">${ing.unit}</span>
-    ${ing.desciption}
-      </div>
-    </li>
-      `;
-      })
-      .join('')}
+    ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
      </div>
 
   <div class="recipe__directions">
@@ -85,23 +93,36 @@ class RecipeView {
     <p class="recipe__directions-text">
       This recipe was carefully designed and tested by
       <span class="recipe__publisher">The Pioneer Woman${
-        recipe.publisher
+        this.#data.publisher
       }/span>. Please check out
       directions at their website.
     </p>
     <a
       class="btn--small recipe__btn"
-      href="${recipe.sourceUrl}"
+      href="${this.#data.sourceUrl}"
       target="_blank"
     >
       <span>Directions</span>
       <svg class="search__icon">
-        <use href="src/img/icons.svg#icon-arrow-right"></use>
+        <use href="${icons}#icon-arrow-right"></use>
       </svg>
     </a>
   </div>`;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentElement('afterbegin', markup);
+  }
+  #generateMarkupIngredient(ing) {
+    return `<li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${icons}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+    }</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${ing.unit}</span>
+  ${ing.desciption}
+    </div>
+  </li>
+    `;
   }
 }
 
