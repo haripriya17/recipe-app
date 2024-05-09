cbf22291123382f1540a81c085a4c82b0c909ec3
haripriya17 <haripriyamekala517@gmail.com>
2024-05-09 16:32:05 -0500

    updated recipe servings

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -63,6 +63,14 @@ class RecipeView extends View {
     ['hashchange', 'load'].forEach(e => window.addEventListener(ev, handler));
   }
 
+  addHandlerUpdateServings(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+      const { updateTo } = +btn.dataset;
+      if (+updateTo > 0) handler(+updateTo);
+    });
+  }
   _generateMarkup() {
     return `
   <figure class="recipe__fig">
@@ -94,12 +102,16 @@ class RecipeView extends View {
       <span class="recipe__info-text">servings</span>
 
       <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
+        <button class="btn--tiny btn--update-servings" data-update-to="${
+          this._data.servings - 1
+        }">
           <svg>
-            <use href="#icon-minus-circle"></use>
+            <use href="${icons}#icon-minus-circle"></use>
           </svg>
         </button>
-        <button class="btn--tiny btn--increase-servings">
+        <button class="btn--tiny btn--update-servings" data-update-to=${
+          this._data.servings + 1
+        }>
           <svg>
             <use href="${icons}#icon-plus-circle"></use>
           </svg>
