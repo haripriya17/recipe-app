d3c39f88492b249f78e05c3665fba18f9fd8a0c5
haripriya17 <haripriyamekala517@gmail.com>
2024-05-13 22:00:59 -0500

    implemented bookmarks part 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,9 +60,17 @@ const controlServings = function (newServings) {
   // Update the recipe view
   recipeView.update(model.state.recipe);
 };
+
+const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  recipeView.update(model.state.recipe);
+};
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -71,6 +71,14 @@ class RecipeView extends View {
       if (+updateTo > 0) handler(+updateTo);
     });
   }
+
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
   _generateMarkup() {
     return `
   <figure class="recipe__fig">
@@ -120,13 +128,12 @@ class RecipeView extends View {
     </div>
 
     <div class="recipe__user-generated">
-      <svg>
-        <use href="src/img/icons.svg#icon-user"></use>
-      </svg>
     </div>
-    <button class="btn--round">
+    <button class="btn--round btn--bookmark">
       <svg class="">
-        <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+        <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-filled' : ''
+    }"></use>
       </svg>
     </button>
   </div>
